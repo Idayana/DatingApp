501dc8ca3896fb166ce76d6bd4c5290a655906a1
Idayana <idayana11@gmail.com>
2019-06-18 16:05:38 +0200

    End of Section 16

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -12,8 +12,7 @@ export class MemberListResolver implements Resolve<User[]> {
     pageNumber = 1;
     pageSize = 5;
 
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService,
-                private authService: AuthService) {}
+    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
         return this.userService.getUsers(this.pageNumber, this.pageSize)
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -32,6 +32,8 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 
 export function tokenGetter(): string {
@@ -51,6 +53,7 @@ export function tokenGetter(): string {
       MemberDetailComponent,
       MemberEditComponent,
       PhotoEditorComponent,
+      MemberMessagesComponent,
       TimeAgoPipe
    ],
    imports: [
@@ -84,6 +87,7 @@ export function tokenGetter(): string {
       MemberListResolver,
       ListsResolver,
       MemberEditResolver,
+      MessagesResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
